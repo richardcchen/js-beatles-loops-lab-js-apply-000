cb489671a70446c832d5059c72a5c5950bafd80c
Richard Chen <richard.c.chen@gmail.com>
2018-07-04 19:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function theBeatlesPlay(musicans, instruments){
   var newarray;
+  for (let i = 0; i < musicans.length; i++){
+    newarray[i] = `${musicans[i]} plays ${instruments[i]}`
+  }
+  return newarray;
   
 }
